e7aa42434193e014465cdab13f0f8e76d14a3dfc
Isabella Bicudo de Souza <isabellabicudo@hotmail.com>
2023-11-15 20:23:03 -0300

    design home v1

--- resources/views/components/padrao.blade.php
+++ resources/views/components/padrao.blade.php
@@ -16,12 +16,20 @@
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" 
         crossorigin="anonymous">
     </script>
+    <!-- CSS stylesheet -->
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/favicon.png" type="image/x-icon">
+    
     <title>{{$title}}</title>
 </head>
 <body>
     
     <x-navbar />
     
+    <x-carousel />
+
+    <x-cards-categorias />
 
     {{ $slot }}
 
