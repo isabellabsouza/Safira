f52ed18dd5150df43b7991ec7a5fa4ee02a4b429
Isabella Bicudo de Souza <isabellabicudo@hotmail.com>
2023-11-19 17:33:04 -0300

    banner

---
+++ resources/views/components/banner-facilidades.blade.php
@@ -0,0 +1,45 @@
+<section class="pb-4 bg-black text-bg-dark mt-4">
+    <h2 class="text-center">Conheça todas as nossas facilidades</h2>
+
+    <div class="d-flex flex-column flex-lg-row align-items-center justify-content-center g-3 px-3">
+        <div class="div-facilidades d-flex align-items-center">
+            <div>
+                <i class="bi bi-x-diamond fs-1 verde-limao"></i>
+            </div>
+            <div>
+                <div class="ms-3 mb-1 verde-limao">PAGUE PELO PIX</div>
+                <div class="texto-menor ms-3">Ganhe 5% OFF em pagamentos via pix</div>
+            </div>
+        </div>
+
+        <div class="div-facilidades d-flex align-items-center">
+            <div>
+                <i class="bi bi-arrow-repeat verde-limao fs-1"></i>
+            </div>
+            <div>
+                <div class="ms-3 mb-1 verde-limao">TROCA GRÁTIS</div>
+                <div class="texto-menor ms-3">Fique livre para trocar em até 30 dias</div>
+            </div>
+        </div>
+
+        <div class="div-facilidades d-flex align-items-center">
+            <div>
+                <i class="bi bi-flower1 fs-1 verde-limao"></i>
+            </div>
+            <div>
+                <div class="ms-3 mb-1 verde-limao">SUSTENTABILIDADE</div>
+                <div class="texto-menor ms-3">Moda sustentável que respeita o meio ambiente
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<form class="border border-secondary my-3 my-xl-5 p-3 text-center div-novidades mx-auto">
+    <h5>Quer receber nossas novidades, promoções exclusivas e 10% OFF na primeira compra? Cadastre-se!</h5>
+    <div class="input-group my-3">
+        <input class="form-control border-secondary rounded-0" type="email" placeholder="Digite seu e-mail"
+            aria-label="Digite seu e-mail" aria-describedby="button-addon2" />
+        <button class="btn btn-primary botao-lilas rounded-0 border-0 " type="button" id="button-addon2">Enviar</button>
+    </div>
+</form>
