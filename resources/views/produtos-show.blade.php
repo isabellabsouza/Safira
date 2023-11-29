f63d0da8769ba46a919f129ae4fe960aa62a0a2b
Isabella Bicudo de Souza <isabellabicudo@hotmail.com>
2023-11-28 21:06:24 -0300

    tabela de medidas

--- resources/views/produtos-show.blade.php
+++ resources/views/produtos-show.blade.php
@@ -6,10 +6,10 @@
                 <div>
                     <?php $i = 0; ?>
                     @foreach ($produto->imagemProduto as $imagem)
-                    <img type="button" data-bs-target="#carouselFotosProduto" id="carousel-selector-{{ $i++ }}"
-                        data-bs-slide-to="{{ $loop->index }}"
-                        class="img-fluid mb-2 carousel-thumb {{ $loop->first ? 'active' : '' }}" aria-current="true"
-                        aria-label="Slide 1" src="{{ asset('storage/' . $imagem->caminho) }}">
+                        <img type="button" data-bs-target="#carouselFotosProduto"
+                            id="carousel-selector-{{ $i++ }}" data-bs-slide-to="{{ $loop->index }}"
+                            class="img-fluid mb-2 carousel-thumb {{ $loop->first ? 'active' : '' }}" aria-current="true"
+                            aria-label="Slide 1" src="{{ asset('storage/' . $imagem->caminho) }}">
                     @endforeach
                 </div>
             </div>
@@ -18,31 +18,31 @@
                     <div class="carousel-inner">
                         <div class="imagem-container">
                             @foreach ($produto->imagemProduto as $imagem)
-                            <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
-                                <img src="{{ asset('storage/' . $imagem->caminho) }}" class="d-block w-100"
-                                    alt="{{ $produto->nome }}">
-                            </div>
+                                <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
+                                    <img src="{{ asset('storage/' . $imagem->caminho) }}" class="d-block w-100"
+                                        alt="{{ $produto->nome }}">
+                                </div>
                             @endforeach
                         </div>
                     </div>
-                    @if(count($produto->imagemProduto) > 1)
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselFotosProduto"
-                        data-bs-slide="prev">
-                        <span class="bi bi-chevron-compact-left text-dark fs-4" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselFotosProduto"
-                        data-bs-slide="next">
-                        <span class="bi bi-chevron-compact-right text-dark fs-4" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
+                    @if (count($produto->imagemProduto) > 1)
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselFotosProduto"
+                            data-bs-slide="prev">
+                            <span class="bi bi-chevron-compact-left text-dark fs-4" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselFotosProduto"
+                            data-bs-slide="next">
+                            <span class="bi bi-chevron-compact-right text-dark fs-4" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
                     @endif
                 </div>
             </div>
             <div class="col col-6">
                 <div class="container">
                     <div class="row">
-                        <small class="text-capitalize">{{$produto->categoria}}</small>
+                        <small class="text-capitalize">{{ $produto->categoria }}</small>
                     </div>
                     <div class="row">
                         <div class="col">
@@ -51,16 +51,16 @@
                     </div>
                     <div class="row mt-2 align-items-center">
                         <div class="col col-auto me-2">
-                            <h3>R$ {{number_format($produto->preco, 2, ",", ".")}}</h3>
+                            <h3>R$ {{ number_format($produto->preco, 2, ',', '.') }}</h3>
                         </div>
                         <div class="col text-end">
-                            <sl-rating label="Rating" value="{{random_int(2,5)}}" readonly></sl-rating>
+                            <sl-rating label="Rating" value="{{ random_int(2, 5) }}" readonly></sl-rating>
                         </div>
                     </div>
                     <div class="row mb-3">
                         <div class="col">
-                            <small>Ou em <b>10x</b> de <b>R$ {{number_format($produto->preco/10, 2, ",",
-                                    ".")}}</b></small>
+                            <small>Ou em <b>10x</b> de <b>R$
+                                    {{ number_format($produto->preco / 10, 2, ',', '.') }}</b></small>
                         </div>
                     </div>
                     <x-divider />
@@ -71,14 +71,62 @@
                     </div>
                     <div class="row">
                         <div class="col">
-                            <form action="{{ route('carrinho.store') }}" method="post" enctype="multipart/form-data">
+                            <button type="button" class="btn botao-medidas btn-sm rounded-0" data-bs-toggle="modal"
+                                data-bs-target="#modalTabelaMedidas">
+                                <i class="bi bi-rulers me-1"></i>
+                                Tabela de Medidas
+                            </button>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col mt-3">
+                            <div class="btn-group me-2" role="group" aria-label="First group">
+                                <button type="button" class="btn btn-outline-secondary rounded-0 me-2">P</button>
+                                <button type="button" class="btn btn-outline-secondary rounded-0 me-2">PP</button>
+                                <button type="button" class="btn btn-outline-secondary rounded-0 me-2">M</button>
+                                <button type="button" class="btn btn-outline-secondary rounded-0 me-2">G</button>
+                                <button type="button" class="btn btn-outline-secondary rounded-0 me-2">GG</button>
+                            </div>
+                            <form class="mt-3" action="{{ route('carrinho.store') }}" method="post"
+                                enctype="multipart/form-data">
                                 @csrf
                                 <input type="hidden" name="produto_id" value="{{ $produto->id }}">
                                 <input type="hidden" name="quantidade" value="1">
-                                <button class="btn btn-primary rounded-1 fs-5" type="submit">Adicionar ao Carrinho <i
-                                        class="bi bi-cart2"></i></button>
+                                <button class="btn botao-lilas rounded-0 fs-5" type="submit">Adicionar ao Carrinho <i
+                                        class="bi bi-bag-fill"></i></button>
                             </form>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="modalTabelaMedidas" tabindex="-1" aria-labelledby="modalTitulo" aria-hidden="true">
+            <div class="modal-dialog modal-lg modal-dialog-centered">
+                <div class="modal-content rounded-0 p-3">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5 text-center w-100" id="modalTitulo">Tabela de Medidas</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p>A tabela deve servir de guia para que você possa escolher a numeração adequada.
+                            Ela não contempla a medida exata dos produtos.</p>
+
+                        <img class="img-fluid" src="/assets/tabela-medidas.jpg" alt="">
+
+                        <p>VEJA COMO MEDIR SEU CORPO:</p>
+
+                        <p>BUSTO: Passe a fita métrica pelas costas e meça na altura dos mamilos.</p>
+
+                        <p>
+                            CINTURA Passe a fita métrica em volta do seu abdômen, acima do osso do quadril, na medida
+                            mais fina.
+                        </p>
+
+                        <p>
+                            QUADRIL Na parte mais larga do bumbum, mais ou menos na metade da altura, contorne o quadril
+                            com a fita métrica.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -145,7 +193,7 @@
                 <div class="row justify-content-center mb-5">
                     {{-- Outros produtos --}}
                     @foreach ($produtosRecomendados as $produto)
-                    <x-smallcard-produtos :produto="$produto" />
+                        <x-smallcard-produtos :produto="$produto" />
                     @endforeach
                 </div>
             </div>
