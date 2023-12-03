bd7b1c46b9a330d5d6b54f9570dfad722a21363a
nicolasarvani <nicolas.arvani@gmail.com>
2023-12-03 20:58:51 -0300

    Refactor form layout in produtos-create.blade.php

--- resources/views/produtos-create.blade.php
+++ resources/views/produtos-create.blade.php
@@ -1,30 +1,43 @@
 <x-padrao title="Novo Produto">
     <x-navbar />
-
-    <form action="" method="post" enctype="multipart/form-data">
+    <div class="container w-50 mt-3">
+        <div class="row text-center">
+            <h2>Novo Produto</h2>    
+        </div>
+        <form action="" method="post" enctype="multipart/form-data">
         @csrf
-        <div>
+        <div class="row mb-3">
             <x-input-label for="nome" :value="__('Nome')" />
             <x-text-input id="nome" type="text" name="nome" :value="old('nome')" required autofocus />
         </div>
-        <div>
+        <div class="row mb-3">
             <x-input-label for="descricao" :value="__('Descrição')" />
             <x-text-input id="descricao" type="text" name="descricao" :value="old('descricao')" required autofocus />
         </div>
-        <div>
+        <div class="row mb-3">
             <x-input-label for="preco" :value="__('Preço')" />
             <x-text-input id="preco" type="number" name="preco" :value="old('preco')" required autofocus />
         </div>
-        <div>
+        <div class="row mb-3">
             <x-input-label for="categoria" :value="__('Categoria')" />
-            <x-text-input id="categoria" type="text" name="categoria" :value="old('categoria')"  autofocus />
+            @php
+                $categorias = ['camisetas', 'calcas', 'vestidos', 'moletom', 'shorts', 'acessorios'];
+            @endphp
+            <select class="form-select" name="categoria" id="categoria">
+                @foreach ($categorias as $categoria)
+                    <option value="{{$categoria}}">{{$categoria}}</option>
+                @endforeach
+            </select>
         </div>
-        <div>
+        <div class="row mb-3">
             <x-input-label for="imagens" :value="__('Imagens')" />
             <x-text-input id="imagens" type="file" name="imagens[]" accept="image/gif, image/jpeg, image/png" multiple autofocus />
             
         </div>
-
-        <button class="btn btn-primary">Salvar</button>
+        <div class="row mb-3">
+            <button class="btn botao-lilas">Salvar</button>
+        </div>
     </form>
+</div>
+
 </x-padrao>
