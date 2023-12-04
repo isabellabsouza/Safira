38d2b4f88adef371f26a27ad879f025896a94174
nicolasarvani <nicolas.arvani@gmail.com>
2023-12-04 08:38:56 -0300

    Refactor category filtering in ProdutoController.php

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -150,38 +150,38 @@ class ProdutoController extends Controller
 
     public function camisetas()
     {
-        $produtos = Produto::all()->where('categoria', 'camisetas');
+        $produtos = Produto::all()->where('categoria', 'camisetas')->where('status', 'ativo');
         //dd($produtos);
         return view('categorias.camisetas')->with('produtos', $produtos);
     }
 
     public function calcas()
     {
-        $produtos = Produto::where('categoria', 'calcas')->get();
+        $produtos = Produto::where('categoria', 'calcas')->where('status', 'ativo')->get();
         return view('categorias.calcas')->with('produtos', $produtos);
     }
 
     public function shorts()
     {
-        $produtos = Produto::where('categoria', 'shorts')->get();
+        $produtos = Produto::where('categoria', 'shorts')->where('status', 'ativo')->get();
         return view('categorias.shorts')->with('produtos', $produtos);
     }
 
     public function vestidos()
     {
-        $produtos = Produto::where('categoria', 'vestidos')->get();
+        $produtos = Produto::where('categoria', 'vestidos')->where('status', 'ativo')->get();
         return view('categorias.vestidos')->with('produtos', $produtos);
     }
 
     public function moletom()
     {
-        $produtos = Produto::where('categoria', 'moletom')->get();
+        $produtos = Produto::where('categoria', 'moletom')->where('status', 'ativo')->get();
         return view('categorias.moletom')->with('produtos', $produtos);
     }
 
     public function acessorios()
     {
-        $produtos = Produto::where('categoria', 'acessorios')->get();
+        $produtos = Produto::where('categoria', 'acessorios')->where('status', 'ativo')->get();
         return view('categorias.acessorios')->with('produtos', $produtos);
     }
 }
